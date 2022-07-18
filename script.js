fad0b7f362e1d27770f0e818563fe174127e6844
Biggest.Deb <alextross15@gmail.com>
2022-07-17 21:04:50 -0400

    Missed a couple commits. Users interface is set up for the most part...

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 //This is a practice rock paper scissors game
+const choices = ["Rock", "Paper", "Scissors"];
 
-console.log("Hello  motha fucka")
+function onClick() {
+    let computerChoice = document.getElementById('cpu-choice').innerText;
+    
+    
+
+    document.getElementById("choice-area").style.display = "flex";
+}
+
+//Gets computers random choice
+function getComputerChoice(){
+    var choice = choices[Math.floor(Math.random() * choices.length)];
+    cpuChoice = choice;
+
+    return(cpuChoice);
+}
+
