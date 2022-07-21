9983e69078727bdfe58682120cc157a0674a7422
Biggest.Deb <alextross15@gmail.com>
2022-07-20 21:52:35 -0400

    Base logic for choosing rock and round ui created.

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 //This is a practice rock paper scissors game
-const choices = ["Rock", "Paper", "Scissors"];
-
-function onClick() {
-    let computerChoice = document.getElementById('cpu-choice').innerText;
-    
-    
-
-    document.getElementById("choice-area").style.display = "flex";
-}
+const choices = ["Rock", "Paper", "Scissors"]
+const rock = false;
+const paper = false;
+const scissors = false;
+let computerChoice;
 
 //Gets computers random choice
 function getComputerChoice(){
     var choice = choices[Math.floor(Math.random() * choices.length)];
     cpuChoice = choice;
 
+    let computer = cpuChoice.toUpperCase();
+    document.getElementById("cpuchoice").innerText = computer;
+
     return(cpuChoice);
 }
+
+//Play button is clicked
+function onPlayClick() {
+    document.getElementById("choice-area").style.display = "flex";
+    document.getElementById("play-btn").disabled = 'true';
+}
+
+function rockClick() {
+    let computer = getComputerChoice();
+    let player = "Rock";
+
+    console.log(computer);
+
+    document.getElementById("rock").disabled = 'true';
+    document.getElementById("paper").disabled = 'true';
+    document.getElementById("scissors").disabled = 'true';
+    document.getElementById("player-choice").innerText = player.toUpperCase();
+    
+    if (computer === player){
+        console.log("tie");
+    } else if (computer === "Paper"){
+        console.log("you lost")
+    } else if (computer === "Scissors"){
+        console.log("WINNER")
+    }    
+}
+
+function logic(player, compuer) {
+    
+
+}
+ 
+
+
+
+
 
